05a36a619f1aad6e7c7f5eb0a3511e0bf2082e78
Lukas <25265369+lukszi@users.noreply.github.com>
2020-07-29 22:20:01 +0200

    Simplify DOMQueries since I found a better way

--- src/DomStuff.js
+++ src/DomStuff.js
@@ -5,35 +5,13 @@
  * @return {HTMLDivElement[]} all movie cards in the given row, ie divs with the class "title-card-container"
  */
 export function getCardsFromRow(row){
-    let rowContainer_title_card = getChildWithClass(row, "rowContainer_title_card");
-    if(!rowContainer_title_card){
+    // I have not yet implemented bigRows, check for that
+    if(!row.querySelector(".rowContainer_title_card")){
         console.log("Detected bigRow, please implement");
         return null;
     }
-    let ptrack_container = getChildWithClass(rowContainer_title_card, "ptrack-container");
-    let rowContent = getChildWithClass(ptrack_container, "rowContent");
-    let slider = getChildWithClass(rowContent, "slider");
-    let sliderMask = getChildWithClass(slider, "sliderMask");
-    let sliderContent = getChildWithClass(sliderMask, "sliderContent");
 
-    // Filter out the slider-items containing actual data
-    let slider_items = Array.from(sliderContent.children)
-        .filter(/** @param {HTMLDivElement} child **/child =>
-            getChildWithClass(child, "title-card-container") !== undefined);
-
-    // Get the title-card from each slider
-    return slider_items.map(sliderItem => getChildWithClass(sliderItem, "title-card-container"));
-}
-
-/**
- *
- * @param {HTMLDivElement} element whose children to search
- * @param {String} className the childElement needs to have
- * @return {HTMLDivElement} first child of element with the given class
- */
-function getChildWithClass(element, className) {
-    return Array.from(element.children)
-        .find(/** @param {HTMLDivElement} child **/child => Array.from(child.classList).includes(className));
+    return row.querySelectorAll(".title-card-container")
 }
 
 /**
@@ -47,10 +25,7 @@ export function getTitleNameFromCard(cardContainer){
         throw "Can't get title from a non 'title-card-container' div.";
     }
 
-    let titleCard = getChildWithClass(cardContainer, "title-card");
-    let ptrack_content = getChildWithClass(titleCard, "ptrack-content");
-    let titleHref = ptrack_content.children[0];
-    return titleHref.text;
+    return cardContainer.querySelector(".ptrack-content").children[0].text;
 }
 
 /**
